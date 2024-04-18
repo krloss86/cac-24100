adfecc5e28bfea2ee019729e79902bded9f416cc
carlos horacio lopez <carloshoracio.lopez@comafi.com.ar>
2024-04-18 18:31:29 -0300

    intro a js

---
+++ js/holamundo.js
@@ -0,0 +1,11 @@
+/*solo puedo escribir javascript */
+/*alert ("") > viene de fabrica*/
+
+//podemos decir que nombre es una variable
+//que va a mutar en el tiempo
+let nombre = prompt("¿Nombre?");
+//en la linea de arriba asigno a nombre el valor ingresado por teclado
+// 2 = 2 * 1;
+
+//variable: 
+alert(nombre);
---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*<script>*/
+    let deporte = prompt("¿Cuál es su deporte favorito?")
+    //"null"
+
+    //alert(deporte)
+    let edad = prompt("¿A qué edad comenzó?")
+    //"null"
+
+    //alert(edad)
+    let club = prompt("¿Qué club le gusta?")
+    //alert(club)
+    //"null"
+
+    //concatenar en UNA SOLA VARIABLE
+    alert(deporte + ' ' + edad + ' ' + club);
+/*</script>*/
