82cd5ae62eb96ba34b863e730f88693e294d292d
carlos horacio lopez <carloshoracio.lopez@comafi.com.ar>
2024-04-23 19:58:11 -0300

    operadores + ejemplo

--- js/buscador.js
+++ js/buscador.js
@@ -1,17 +1,22 @@
 //obtengo la referencia del tag botton en html
 //como un objeto en js
-const butonEnHTML = document.getElementById('btnBusqueda');
+const butonEnHTML = capturarPorId('btnBusqueda');
 
 //creo la fucion y
 function buscar() {
 
-    const claveEnHTML = document.getElementById('claveDeBusqueda');
+    const claveEnHTML = capturarPorId('claveDeBusqueda');
     
     //enviar al HTML, dentro del h4, el contenido del input
-    const h4EnHTML = document.getElementById('textoBuscado');
+    const h4EnHTML = capturarPorId('textoBuscado');
 
     //concatenar dos string
     h4EnHTML.innerText = 'Encontramos 2 items para ' + claveEnHTML.value;
+}
+//se supone que htmlID representa:
+//btnBusqueda o claveDeBusqueda o textoBuscado
+function capturarPorId(htmlId) {
+    return document.getElementById(htmlId);    
 }
 
 //asigno al boton Buscar un evento click=onclick
---
+++ js/operadores.js
@@ -0,0 +1,36 @@
+//1 crear la funcion
+function calcular() {
+    //2 obtengo valor de input
+    const valorA = document.getElementById('valorA').value;
+    
+    //funcion de fabrica parseInt/parseFloat
+    //const a = parseFloat(valorA);
+    //const a = +valorA;
+    const a = Number(valorA);
+
+    const valorB = document.getElementById('valorB').value;
+    const b = Number(valorB);
+
+    const resultado = document.getElementById('resultado');
+
+    //saber si a > b
+    if(a > b) {        
+        resultado.innerText = 'A es > B';
+    }
+
+    //si a === b
+    if(a === b) {
+       resultado.innerText = 'A es = a B'; 
+    }
+
+    // si a < b
+    if(a < b ) {
+        resultado.innerText = 'A es < que B'
+    }
+}
+
+// if/else
+// switch
+// ciclos:
+// for/white/do-while
+// arrays
